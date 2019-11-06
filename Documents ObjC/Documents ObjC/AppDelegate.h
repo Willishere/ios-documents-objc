7cf2e07ad2aafff89d6a3b91a42cad834734fab5
William <chen.will7@gmail.com>
2019-11-06 13:17:45 -0800

    added file!

---
+++ Documents ObjC/Documents ObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Documents ObjC
+//
+//  Created by William Chen on 11/6/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Documents ObjC/Documents ObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Documents ObjC
+//
+//  Created by William Chen on 11/6/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
